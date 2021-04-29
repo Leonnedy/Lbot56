af8b7c486480c639720ca893aff8ffde90bc45e5
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-04-29 19:11:06 -0400

    Add files via upload

---
+++ lista/animelist.js
@@ -0,0 +1,47 @@
+const animelista = (prefix) => {
+
+	return `
+ 💪🏻𝙰𝚀𝚄𝙸 𝙴 𝙾 𝚇𝙰𝙽𝙳Ã𝙾 𝙿𝙾𝚁𝚁𝙰💪🏻
+
+ ╭╮╮╱▔▔▔▔╲╭╭╮ 
+ ╰╲╲▏▂╲╱▂▕╱╱╯
+ ┈┈╲▏▇▏▕▇▕╱┈┈
+ ┈┈╱╲▔▕▍▔╱╲┈┈ 
+ ╭╱╱ ▕╋╋╋╋▏╲╲╮
+ ╰╯╯┈╲▂▂╱┈╰╰ 
+ 
+  𝙿𝚎𝚒𝚝𝚊 𝚘𝚞 𝚁𝚎𝚜𝚙𝚎𝚒𝚝𝚊 𝙼𝚎𝚛𝚘 𝙼𝚘𝚛𝚝𝚊𝚕
+             HAHAHAHA😜
+ 
+ 👇 *Esta es la lista de anime que he visto:*👇
+  
+_Nanatsu No Taizai: cuenta la triste y divertida historia de Meliodas y los siete pecados capitales_
+https://animesonline.cc/anime/nanatsu-no-taizai-hd/
+
+
+_Undefeated Bahamut: cuenta la historia de Lux Arcadia quien tiene una espada que lo cubre con la armadura más fuerte del mundo_
+https://animesonline.games/b/animes/undefeated-bahamut-chronicle
+
+
+_No Game no Life: cuenta la historia de dos hermanos Shiro y Sora, son la pareja de jugadores invencibles llamados Kuhako_
+https://animesonline.cc/temporada/no-game-no-life-1-temporada-online/
+
+
+_DanMachi: cuenta la historia de Bell Cranell, su diosa Hestia y sus amigos, su sueño y siendo un héroe, conoce a una chica muy hermosa de la que se enamora_
+https://animesonline.cc/anime/dungeon-ni-deai-o-motomeru-no-wa-machigatteiru-darou-ka/
+
+
+_Boku no Hero academia:Cuenta la historia de Izuku Midoriya, sueña con ser el héroe más poderoso de todos, cuanto más nació sin un poder llamado individualidad, más gamha uno de sus héroes favoritos._
+https://www.myanimesonline.biz/animes/boku-no-hero-academia/
+
+
+_To Love Ru: cuenta la historia PERVERTIDA Y AFORTUNADA de Yuuki Rito, su SUERTE comienza cuando un alienígena HOT TO FUCK aparece DESPELADO en su bañera_, *No voy a dar spoiler asi que cuida tu salafraio*
+https://animesync.biz/anime/to-love-ru-hd/
+
+DIVIÉRTETE Y ENTRENA TU BRAZO SI ES QUE ME ENTIENDE HEHEHE
+
+
+El último anime es para que ejercites tu brazo muy delgado.
+}
+exports.animelist = animelist
+
