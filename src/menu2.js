93c11074c5f4702fe6f789e8794866e762fdb1c4
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-04-29 19:09:47 -0400

    Add files via upload

---
+++ src/menu2.js
@@ -0,0 +1,50 @@
+const menu2 = (prefix) => { 
+	return `                 
+╠══✪〘 MENU 〙✪══
+║
+╠➥ *${prefix}figu*
+╠➥ *${prefix}toimg*
+╠➥ *${prefix}meme*
+╠➥ *${prefix}memeindo*
+╠➥ *${prefix}tts*
+╠➥ *${prefix}loli [off]*
+╠➥ *${prefix}nsfwloli [off]*
+╠➥ *${prefix}url2img*
+╠➥ *${prefix}lentes [en la leyenda]*
+╠➥ *${prefix}espera [en la leyenda]*
+╠➥ *${prefix}setprefix*
+║
+╠══✪〘 IMAGENES 〙✪══
+║
+╠➸ *${prefix}loli* [off]
+╠➸ *${prefix}loli1*
+╠➸ *${prefix}hentai*
+╠➸ *${prefix}dono*
+╠➸ *${prefix}porno*
+╠➸ *${prefix}buenasnoches*
+╠➸ *${prefix}buendia*
+╠➸ *${prefix}buenastardes*
+╠➸ *${prefix}mia*
+╠➸ *${prefix}mia1*
+╠➸ *${prefix}mia2*
+╠➸ *${prefix}belle*
+╠➸ *${prefix}belle1*
+╠➸ *${prefix}belle2*
+╠➸ *${prefix}belle3*
+╠➸ *${prefix}akeno*
+╠➸ *${prefix}meme*   
+╠➸ *${prefix}lofi*
+╠➸ *${prefix}malkova*
+╠➸ *${prefix}canal*
+╠➸ *${prefix}nsfwloli1*
+╠➸ *${prefix}reislin*
+║
+╠══✪〘 INTELIGENCIA ARTIFICIAL 〙✪══
+║
+╠➸ *${prefix}simih 1 (para ativar)*
+╠➸ *${prefix}simih 0 (para desactivar)*
+╠➥ *${prefix}simi (su mensaje)*
+║
+╠══✪〘 *CAUSA BOT* 〙✪══`
+}
+exports.menu2 = menu2
---
+++ src/outros.js
@@ -0,0 +1,41 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 *OTROS* 〙✪══
+║
+╠➥ *${prefix}linkgp*
+╠➥ *${prefix}simih [1/0]*
+╠➥ *${prefix}marcar*
+╠➥ *${prefix}add [@]*
+╠➥ *${prefix}ban [@]*
+╠➥ *${prefix}promover [@]*
+╠➥ *${prefix}rebajar*
+╠➥ *${prefix}admins*
+╠➥ *${prefix}marcar2*
+╠➥ *${prefix}bc [texto]* (ele faz uma ™)
+╠➥ *${prefix}marcar3*
+╠➥ *${prefix}bloqueados*
+╠➥ *${prefix}bloquear [@]*
+╠➥ *${prefix}desbloquear [@]*
+╠➥ *${prefix}limpar*
+╠➥ *${prefix}bc [ *texto* ]*
+╠➥ *${prefix}bemvindo [1/0]*
+╠➥ *${prefix}clonar [@]*
+╠➥ *${prefix}help1*
+╠➥ *${prefix}dono*
+╠➥ *${prefix}owner*
+╠➥ *${prefix}tts [texto]*
+╠➥ *${prefix}setnome*
+╠➥ *${prefix}termux*
+╠➥ *${prefix}setfoto*
+╠➥ *${prefix}grupoinfo*
+╠➥ *${prefix}ytmp4*
+╠➥ *${prefix}bomdia*
+╠➥ *${prefix}buenanoches*
+╠➸ *${prefix}marcar*
+╠➸ *${prefix}marcar2*
+╠➸ *${prefix}marcar3*
+║
+╚═〘  *DARKBOT* 〙
+`
+}
+exports.iklan = iklan
